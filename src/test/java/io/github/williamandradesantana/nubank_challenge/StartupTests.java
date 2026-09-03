af8cbeb97da1103b05b485ac5a89b9b0941a4a78
William Santana <williamandrade1058@gmail.com>
2026-09-03 14:58:07 -0300

    initial commit

---
+++ src/test/java/io/github/williamandradesantana/nubank_challenge/StartupTests.java
@@ -0,0 +1,13 @@
+package io.github.williamandradesantana.nubank_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
