68302030246ad6c148b507291ea317b0e29687aa
William Santana <williamandrade1058@gmail.com>
2026-09-05 09:37:18 -0300

    test: configura infraestrutura de testes de integração com Testcontainers

---
+++ src/test/java/io/github/williamandradesantana/nubank_challenge/PostgresIntegrationTest.java
@@ -0,0 +1,16 @@
+package io.github.williamandradesantana.nubank_challenge;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@ActiveProfiles("test")
+public abstract class PostgresIntegrationTest {
+
+    @ServiceConnection
+    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16-alpine");
+
+    static {
+        POSTGRES.start();
+    }
+}
